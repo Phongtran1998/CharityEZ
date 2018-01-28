7c2ff1d43ca96949973c987461ac09826f9dc910
[Phongtran1998] <hoang_phong98@me.com>
2018-01-27 20:39:39 -0500

    Updated Account.vue

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,5 +1,40 @@
 <template>
-    <div>ghghg</div>
+  <div class="main-wrapper">
+    <div class="internal-wrap">
+      <div class="deep-wrap1">
+        <h2>My Bank Account</h2>
+        <p>Available Balance:</p>
+        <p>Account Number:</p>
+        <p>Routing Numbers:</p>
+        <p>Average Spending:</p>
+      </div>
+      <div class="deep-wrap1">
+        <h2><br></h2>
+        <p>1000</p>
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
+      <div class="deep-wrap2">
+        <h2><br></h2>
+        <button>Set Up</button>
+      </div>
+    </div>
+    <div class="internal-wrap">
+      <h2>Choose the Organizations to donate</h2>
+      <h3>Choose the category</h3>
+      <select>
+        <option selected>...</option>
+        <option>Education</option>
+        <option>Animal</option>
+        <option>Environment</option>
+        <option>International NGOs</option>
+        <option>Health</option>
+        <option>Arts and Culture</option>
+      </select>
+
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +44,69 @@
 </script>
 
 <style scoped>
+/*p {*/
+  /*text-align: left;*/
+
+/*}*/
+/*h2 {*/
+  /*text-align: left;*/
+
+/*}*/
+/*h3 {*/
+  /*text-align: left;*/
+
+/*}*/
+
+  .main-wrapper{
+    width: 100%;
+    display: flex;
+
+  }
+h2 {
+  font-size: 35px;
+  font-family: Karla,serif;
+}
+
+p {
+  font-size: 35px;
+  font-family: Sofia;
+}
+
+  .internal-wrap:first-child {
+    margin-left: 60px;
+    width: 50%;
+    display: inline-flex;
+    text-align: left;
+  }
+  .internal-wrap {
+    margin-left: 60px;
+    width: 50%;
+    display: inline-block;
+    text-align: center;
+    line-height: 100px;
+  }
+  .deep-wrap1 {
+    width: 40%;
+    display: inline-block;
+  }
+  .deep-wrap1:last-child {
+    width: 20%;
+    display: inline-block;
+
+  }
+  .deep-wrap2 {
+    width: 33%;
+    display: inline-block;
+    text-align: left;
+  }
+  button {
+    position: absolute;
+    width: 100px;
+  }
+  select {
+    width: 200px;
+    ;
+  }
+
 
 </style>
